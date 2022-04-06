42941cdddc093bb7939f78eccc85594cf022aae7
polatengin <enpolat@microsoft.com>
2022-04-06 16:14:56 -0700

    adding else clause

--- .az_bicep_generate.sh
+++ .az_bicep_generate.sh
@@ -3,5 +3,8 @@ function az() {
   # check only for the arguments that we want to extend
   if [[ ${1} == "bicep" && ${2} == "generate" ]]; then
 
+  # if the command is not the one we want to extend, pass all the arguments to the command
+  else
+    command az "$@"
   fi
 }
