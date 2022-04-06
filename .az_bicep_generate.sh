8b2cea29dddf6680559983317dfa272eefa4f367
polatengin <enpolat@microsoft.com>
2022-04-06 16:15:24 -0700

    if not required parameters passed, display usage message

--- .az_bicep_generate.sh
+++ .az_bicep_generate.sh
@@ -3,6 +3,13 @@ function az() {
   # check only for the arguments that we want to extend
   if [[ ${1} == "bicep" && ${2} == "generate" ]]; then
 
+    # if required arguments are present, execute the command
+    if [[ ${3} == "--resource-group" || ${3} == "-n" ]]; then
+
+    # if required arguments are not present, print the usage message
+    else
+      echo "Usage: az bicep generate --resource-group <resource-group-name> [--output-file <file-name>]";
+    fi
   # if the command is not the one we want to extend, pass all the arguments to the command
   else
     command az "$@"
