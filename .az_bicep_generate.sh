0e633f3e58304a5de56a995f6497b4f4cc5f0b25
polatengin <enpolat@microsoft.com>
2022-04-06 16:14:29 -0700

    checking if the desired command with subcommands called

--- .az_bicep_generate.sh
+++ .az_bicep_generate.sh
@@ -1,3 +1,7 @@
 # to extend a command, start with a function that has the same name as the command
 function az() {
+  # check only for the arguments that we want to extend
+  if [[ ${1} == "bicep" && ${2} == "generate" ]]; then
+
+  fi
 }
